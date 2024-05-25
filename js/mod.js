feb4ca37d4e0ff38983c0d29d6bff7ca197737f5
etl1208 <thianlereng@gmail.com>
2024-05-25 20:47:47 +0800

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -14,7 +14,7 @@ let modInfo = {
 // Set your version in num and name
 let VERSION = {
 	num: "0.0",
-	name: "Literally nothing",
+	name: "Hardest tree",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
@@ -56,7 +56,7 @@ var displayThings = [
 
 // Determines when the game "ends"
 function isEndgame() {
-	return player.points.gte(new Decimal("e280000000"))
+	return player.points.gte(new Decimal("ee100"))
 }
 
 
